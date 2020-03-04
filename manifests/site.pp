e703241596349723dd5474e36cf18031a1d23dff
shijithpkd <shijith.pkd@gmail.com>
2020-03-04 11:35:43 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
   file {'/root/README':
     ensure => file,
-    content => 'THis created from GIT hub server by shijith',
+    content => 'THis created from GIT hub server by shijith ntt',
     }
 }
